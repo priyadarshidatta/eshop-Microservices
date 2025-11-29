a295d5831caf0bb25e8ffa3bb279aa3b2b5fdf5f
priyadarshidatta <43062697+priyadarshidatta@users.noreply.github.com>
2025-11-29 16:14:46 +0530

    Create WeatherforecastController.cs with GET method

---
+++ Services/Weather/Controllers/WeatherforecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop_Microservices.Services.Weather.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherforecastController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = summaries[rng.Next(summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public string Summary { get; set; }
+    }
+}
